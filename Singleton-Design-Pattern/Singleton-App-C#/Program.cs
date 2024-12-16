a7a9889029b4a77376fe9b418f42069960f84902
yaseen-asaliya <yaseen.asaliya22@gmail.com>
2024-12-16 20:56:34 +0200

    Add singleton implementation

---
+++ Singleton-Design-Pattern/Singleton-App-C#/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+﻿namespace DesignPatterns
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            // Single singleton
+            Singleton obj = Singleton.GetInstance(); // no matter how much this line invoked, it will always return the same object
+            obj.attr1 = 1; 
+            Console.WriteLine(obj.attr1); 
+
+            // Singleton with registery 
+            ISingleton singleton = SingletonRegistry.Lookup("SingletonA");
+            singleton?.DoWork();
+
+            singleton = SingletonRegistry.Lookup("SingletonB");
+            singleton?.DoWork(); 
+        }
+    }
+}
---
+++ Singleton-Design-Pattern/Singleton-App-C#/Singleton.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPatterns
+{
+
+    // Simple Singleton implementation 
+    public class Singleton
+    {
+        public int attr1;
+        public string attr2;
+
+        private static Singleton instance; // static object, so it will create only one time 
+
+        private Singleton() 
+        {
+            attr1 = 0;
+            attr2 = "";
+        }
+
+        public static Singleton GetInstance() { // static get method (it will create the object first time)
+            if (instance == null) 
+            {
+                instance = new Singleton();
+            } 
+            return instance;
+        }
+    }
+
+    // Advance Singleton implementation (use register)
+    // it's just a way to store set of singleton objects in a dictonary  
+
+    public interface ISingleton
+    {
+        void DoWork(); // A method that all Singleton classes should implement
+    }
+
+   public class SingletonRegistry
+    {
+        private static Dictionary<string, ISingleton> _registry = new Dictionary<string, ISingleton>();
+
+        public static void Register(string name, ISingleton instance)
+        {
+            if (!_registry.ContainsKey(name))
+            {
+                _registry[name] = instance;
+            }
+        }
+
+        public static ISingleton Lookup(string name)
+        {
+            if (_registry.ContainsKey(name))
+            {
+                return _registry[name];
+            }
+            return null;
+        }
+    }
+
+    public class SingletonA : ISingleton
+    {
+        public static SingletonA Instance { get; private set; } = new SingletonA();
+
+        private SingletonA()
+        {
+            SingletonRegistry.Register("SingletonA", this); // Register the instance by name
+        }
+
+        public void DoWork()
+        {
+            Console.WriteLine("SingletonA is doing work.");
+        }
+    }
+
+    public class SingletonB : ISingleton
+    {
+        public static SingletonB Instance { get; private set; } = new SingletonB();
+
+        private SingletonB()
+        {
+            SingletonRegistry.Register("SingletonB", this); // Register the instance by name
+        }
+
+        public void DoWork()
+        {
+            Console.WriteLine("SingletonB is doing work.");
+        }
+    }
+}
