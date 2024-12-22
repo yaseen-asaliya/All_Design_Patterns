df855ac2932257c0b01007023d0cf0391cc514de
yaseen-asaliya <yaseen.asaliya22@gmail.com>
2024-12-22 21:26:25 +0200

    Factory Method Implementation in C#

---
+++ Factory-Method-Design-Pattern/Factory-Method-In-C#/Factory/Cat.cs
@@ -0,0 +1,11 @@
+
+namespace Factory {
+    public class Cat : IAnimal {
+        // Add Cat attributes here...
+
+        public void MakeNoise() {
+            // Cat noise implementation 
+            Console.WriteLine("Cat Noise...");
+        }
+    } 
+}
---
+++ Factory-Method-Design-Pattern/Factory-Method-In-C#/Factory/IAnimal.cs
@@ -0,0 +1,11 @@
+
+
+namespace Factory {
+    public interface IAnimal {
+        void MakeNoise();
+
+        // Add other functions here...
+
+    }
+
+}
